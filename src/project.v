13445bce430774fb0bdcebc4d728a5313dd9b37e
ponesmateo26-coder <ponesmateo26@gmail.com>
2026-09-19 15:54:33 +0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module tt_um_hardware_dice (
+module tt_um_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
